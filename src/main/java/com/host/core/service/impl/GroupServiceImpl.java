b1b5302f8a2c04f4e872201ab2e88bd34a0bd5fb
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-04-24 08:01:50 +0200

    Crete GroupService and UserService

---
+++ src/main/java/com/host/core/service/impl/GroupServiceImpl.java
@@ -0,0 +1,8 @@
+package com.host.core.service.impl;
+
+import com.host.core.service.GroupService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GroupServiceImpl implements GroupService{
+}
---
+++ src/main/java/com/host/core/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.host.core.service.impl;
+
+import com.host.core.service.UserService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserServiceImpl implements UserService{
+}
