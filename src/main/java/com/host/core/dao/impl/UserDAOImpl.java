9f316714b3a5e9513023d0ce2fcf3da55fb8558d
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-04-25 14:37:17 +0200

    Tests GroupDAO

--- src/main/java/com/host/core/dao/impl/UserDAOImpl.java
+++ src/main/java/com/host/core/dao/impl/UserDAOImpl.java
@@ -2,11 +2,15 @@ package com.host.core.dao.impl;
 
 import com.host.core.dao.UserDAO;
 import com.host.core.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Repository
 public class UserDAOImpl implements UserDAO {
@@ -15,31 +19,45 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public User find(Long id) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        Query query = session.createQuery("from User as user where user.id = '" + id.toString() + "'");
+        return (User) query.uniqueResult();
     }
 
     @Override
     public Set<User> find() {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        Set<User> users = (Set<User>) session.createQuery("from User").getResultStream().collect(Collectors.toCollection(HashSet::new));
+        return users;
     }
 
     @Override
     public boolean delete(Long id) {
-        return false;
+        Session session = sessionFactory.getCurrentSession();
+        session.delete(find(id));
+        session.flush();
+        return true;
     }
 
     @Override
-    public boolean add(User object) {
-        return false;
+    public User save(User user) {
+        Session session = sessionFactory.getCurrentSession();
+        session.save(user);
+        return user;
     }
 
     @Override
-    public boolean update(User object) {
-        return false;
+    public User update(User user) {
+        Session session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(user);
+        session.flush();
+        return user;
     }
 
     @Override
     public User find(String username) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        Query query = session.createQuery("from User as user where user.username = '" + username + "'");
+        return (User) query.uniqueResult();
     }
 }
---
+++ src/test/java/com/host/core/dao/GroupDAOTest.java
@@ -0,0 +1,134 @@
+package com.host.core.dao;
+
+import com.host.config.AppConfig;
+import com.host.core.model.Group;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfig.class)
+public class GroupDAOTest {
+    @Autowired
+    private GroupDAO groupDAO;
+    private Group group;
+    private String name1 = "n1";
+    private String name2 = "n2";
+
+    @Before
+    public void before() {
+        group = new Group();
+        deleteGroupsForTests();
+    }
+
+    @After
+    public void after() {
+        deleteGroupsForTests();
+    }
+
+    @Transactional
+    private void deleteGroupsForTests() {
+        if (groupDAO.find(name1) != null) groupDAO.delete(groupDAO.find(name1).getId());
+        if (groupDAO.find(name2) != null) groupDAO.delete(groupDAO.find(name2).getId());
+    }
+
+    @Test
+    @Transactional
+    public void deleteTestingGroup() {
+        groupDAO.save(createGroup(name1));
+        groupDAO.save(createGroup(name2));
+
+        assertThat(groupDAO.find(name1)).isNotNull();
+        assertThat(groupDAO.find(name1)).isNotNull();
+
+        deleteGroupsForTests();
+
+        assertThat(groupDAO.find(name1)).isNull();
+        assertThat(groupDAO.find(name1)).isNull();
+    }
+
+    @Test
+    @Transactional
+    public void saveGroup() {
+        group = groupDAO.save(createGroup(name1));
+
+        assertThat(group.getId()).isNotNull();
+        assertThat(group.getName()).isEqualTo(name1);
+    }
+
+    @Test
+    @Transactional
+    public void deleteGroup() {
+        group = createGroup(name1);
+        groupDAO.save(group);
+
+        assertThat(group.getId()).isNotNull();
+        assertThat(groupDAO.delete(group.getId())).isTrue();
+    }
+
+    @Test
+    @Transactional
+    public void findGroupById() {
+        groupDAO.save(createGroup(name1));
+        group = groupDAO.find(name1);
+
+        assertThat(group.getId()).isNotNull();
+        assertThat(group.getName()).isEqualTo(name1);
+    }
+
+    @Test
+    @Transactional
+    public void findAllGroups() {
+        int size = groupDAO.find().size();
+
+        groupDAO.save(createGroup(name1));
+        groupDAO.save(createGroup(name2));
+
+        assertThat(groupDAO.find().size()).isEqualTo(size + 2);
+    }
+
+    @Test
+    @Transactional
+    public void findGroupByName() {
+        groupDAO.save(createGroup(name1));
+        group = groupDAO.find(name1);
+
+        assertThat(group).isNotNull();
+        assertThat(group.getName()).isEqualTo(name1);
+    }
+
+    @Test
+    @Transactional
+    public void updateGroup() {
+        assertThat(groupDAO.find(name1)).isNull();
+        assertThat(groupDAO.find(name2)).isNull();
+
+        groupDAO.save(createGroup(name1));
+
+        assertThat(groupDAO.find(name1)).isNotNull();
+        assertThat(groupDAO.find(name2)).isNull();
+
+        group = groupDAO.find(name1);
+        group.setName(name2);
+        groupDAO.update(group);
+
+        assertThat(groupDAO.find(name1)).isNull();
+        assertThat(groupDAO.find(name2)).isNotNull();
+    }
+
+    private Group createGroup(String name) {
+        Group group = new Group();
+        group.setName(name);
+        group.setUsers(new HashSet<>());
+        return group;
+    }
+}
