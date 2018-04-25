2d3a35460fa003fe779dc75575179c213cf601d4
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-04-25 13:35:15 +0200

    Make implementation GroupDAO

--- src/main/java/com/host/core/dao/impl/GroupDAOImpl.java
+++ src/main/java/com/host/core/dao/impl/GroupDAOImpl.java
@@ -2,39 +2,63 @@ package com.host.core.dao.impl;
 
 import com.host.core.dao.GroupDAO;
 import com.host.core.model.Group;
+import com.host.core.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Repository
 public class GroupDAOImpl implements GroupDAO {
+    @Autowired
+    private SessionFactory sessionFactory;
+
     @Override
     public Group find(Long id) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        Query query = session.createQuery("from Groups as groups where groups.id = '" + id.toString() + "'");
+        return (Group) query.uniqueResult();
     }
 
     @Override
     public Set<Group> find() {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        Set<Group> groups = (Set<Group>) session.createQuery("from Groups").getResultStream().collect(Collectors.toCollection(HashSet::new));
+        return groups;
     }
 
     @Override
     public boolean delete(Long id) {
-        return false;
+        Session session = sessionFactory.getCurrentSession();
+        session.delete(find(id));
+        session.flush();
+        return true;
     }
 
     @Override
-    public boolean add(Group object) {
-        return false;
+    public Group add(Group group) {
+        Session session = sessionFactory.getCurrentSession();
+        session.save(group);
+        return group;
     }
 
     @Override
-    public boolean update(Group object) {
-        return false;
+    public Group update(Group group) {
+        Session session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(group);
+        session.flush();
+        return group;
     }
 
     @Override
     public Group find(String name) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        Query query = session.createQuery("from Groups as groups where groups.name = '" + name + "'");
+        return (Group) query.uniqueResult();
     }
 }
