bb4033f63010d4b8e5f1ea04ea67b9475989e8fe
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-04-25 13:00:41 +0200

    Config transaction/entityfactory few small fix

--- src/main/java/com/host/config/RepositoryConfig.java
+++ src/main/java/com/host/config/RepositoryConfig.java
@@ -1,10 +1,11 @@
 package com.host.config;
 
-import com.host.core.model.CoreModel;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -23,6 +24,10 @@ public class RepositoryConfig {
     private String username;
     @Value("${jdbc.password}")
     private String password;
+    @Value("${jdbc.pool.size}")
+    private String poolSize;
+    @Value("${jdbc.timeout}")
+    private String timeOut;
 
     @Value("${hibernate.dialect}")
     private String hibernateDialect;
@@ -30,7 +35,6 @@ public class RepositoryConfig {
     private String hibernateShowSql;
     @Value("${hibernate.hbm2ddl.auto}")
     private String hibernateHbm2ddlAuto;
-
 
     private String getUrl() {
         StringBuilder url = new StringBuilder("jdbc:h2:file:.");
@@ -48,17 +52,17 @@ public class RepositoryConfig {
         bds.setUrl(getUrl());
         bds.setUsername(username);
         bds.setPassword(password);
-        bds.setInitialSize(1);
-        bds.setMaxTotal(5);
+        bds.setMaxTotal(Integer.valueOf(poolSize));
+        bds.setMaxWaitMillis(Long.valueOf(timeOut));
         return bds;
     }
 
     @Bean
-    public LocalSessionFactoryBean sessionFactory(DataSource ds) {
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
         LocalSessionFactoryBean sfb = new LocalSessionFactoryBean();
-        sfb.setDataSource(getDataSource());
+        sfb.setDataSource(dataSource);
         sfb.setHibernateProperties(getHibernateProperties());
-        sfb.setPackagesToScan(new String[]{CoreModel.class.getPackage().toString()});
+        sfb.setPackagesToScan("com.host.core.model");
         return sfb;
     }
 
@@ -68,8 +72,14 @@ public class RepositoryConfig {
         properties.setProperty("hibernate.dialect", hibernateDialect);
         properties.setProperty("hibernate.show_sql", hibernateShowSql);
         properties.setProperty("hibernate.hbm2ddl.auto", hibernateHbm2ddlAuto);
-
         return properties;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(sessionFactory);
+        return txManager;
     }
 
 }
